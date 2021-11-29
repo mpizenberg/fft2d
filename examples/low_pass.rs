9938f53ccb0df9b50502ca82a7f6dd7689033329
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2021-11-29 14:41:19 +0100

    Move default implementation into slice module

--- examples/low_pass.rs
+++ examples/low_pass.rs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MPL-2.0
 
-use fft2d::{fft_2d, fftshift, ifft_2d, ifftshift};
+use fft2d::slice::{fft_2d, fftshift, ifft_2d, ifftshift};
 use image::GrayImage;
 use rustfft::num_complex::Complex;
 use show_image::create_window;
